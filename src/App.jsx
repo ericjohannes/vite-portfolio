baf652cc7474405b3a4b982ea9b7795422c5daf3
Eric Blom <eric.j.blom@gmail.com>
2023-01-28 13:32:01 -0600

    new intro stuff

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ const App = ()=>{
       <h1 className='page-title'>Eric Blom</h1>
       <p className='intro'>I'm a senior newsroom developer at National Geographic. Before that I  was a developer straddling editorial and product for <a href="https://protocol.com" target="_blank">Protocol</a> (RIP) and <a href="https://www.sfchronicle.com/" target="_blank">Heasrt Newspapers</a>. I started my career reporting for a <a href="https://rivertonranger.com/" target="_blank">small newspaper in Wyoming</a> but fell in love with coding from taking <a href="https://pll.harvard.edu/course/cs50-introduction-computer-science" target="_blank">CS50</a> online.</p>
       <h2 className='subhead'>Some projects I'm proud of</h2>
-      <p className='disclaimer'>I did my best to set the links to work around paywalls but some workarounds might stop working. And most of the code is proprietary so I'm showing what I can.</p>
+      <p className='disclaimer'>I sought to have the link to work around paywalls but some workarounds might stop working. Also, most of the code is proprietary so I'm showing what I can.</p>
       <section key="top-projects">
               {highPriority.map(handleLink)}
       </section>
