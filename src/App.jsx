1702e1dd615aff9ab217121d79d136df172ba187
Eric Blom <eric.j.blom@gmail.com>
2023-02-04 16:16:37 -0600

    flexbox styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,27 @@ let highPriority = links.filter((link)=>link.priority==0)
 
 const handleLink = (link)=>{
   return (
-  <div key={`"link-${link.name}"`}>            
+  <div key={`"link-${link.name}"`} className="project-link">            
+
+    <div className='photo-wrapper'>
+      <div className='link-photo' style={{backgroundImage: `url('./${link.photo}')`}}> </div>
+    </div>
     <a href={link.link} targetf="_blank">
       <h3>{link.name}</h3>
     </a>
     <p>{link.org}</p>
-    <div className='photo-wrapper'>
-      <div className='link-photo' style={{backgroundImage: `url('./${link.photo}')`}}>
-        
+    <p className='about-text'>{link.about}</p>
 
-      </div>
-      <p className='about-text'>{link.about}</p>
-    </div>
   </div>)
 }
 const App = ()=>{
-  console.log('links',links)
   return (
     <div className="App">
       <h1 className='page-title'>Eric Blom</h1>
       <p className='intro'>I'm a senior newsroom developer at National Geographic. Before that I  was a developer for <a href="https://protocol.com" target="_blank">Protocol</a> (RIP) and <a href="https://www.sfchronicle.com/" target="_blank">Heasrt Newspapers</a>. I started my career reporting for a <a href="https://rivertonranger.com/" target="_blank">small newspaper in Wyoming</a> but fell in love with coding from <a href="https://pll.harvard.edu/course/cs50-introduction-computer-science" target="_blank">CS50</a>, online intro to computer science course.</p>
       <h2 className='subhead'>Some projects I'm proud of</h2>
       {/* <p className='disclaimer'>I sought to have the link to work around paywalls but some workarounds might stop working. Also, most of the code is proprietary so I'm showing what I can.</p> */}
-      <section key="top-projects">
+      <section className="top-projects">
               {highPriority.map(handleLink)}
       </section>
       
