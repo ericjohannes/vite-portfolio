36c9f0723aa706cad9cbc94ca4b611bc1f9c3f99
Eric Blom <eric.j.blom@gmail.com>
2023-02-04 15:40:51 -0600

    keep up to date

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 This is a project to build a portfolio site for myself. It's made with vite.
+
+run `npm run dev` to run dev server
+
+more info https://vitejs.dev/guide/ 
--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const handleLink = (link)=>{
     </a>
     <p>{link.org}</p>
     <div className='photo-wrapper'>
-      <div className='link-photo' style={{backgroundImage: `url('./public/${link.photo}')`}}>
+      <div className='link-photo' style={{backgroundImage: `url('./${link.photo}')`}}>
         
 
       </div>
@@ -24,9 +24,9 @@ const App = ()=>{
   return (
     <div className="App">
       <h1 className='page-title'>Eric Blom</h1>
-      <p className='intro'>I'm a senior newsroom developer at National Geographic. Before that I  was a developer straddling editorial and product for <a href="https://protocol.com" target="_blank">Protocol</a> (RIP) and <a href="https://www.sfchronicle.com/" target="_blank">Heasrt Newspapers</a>. I started my career reporting for a <a href="https://rivertonranger.com/" target="_blank">small newspaper in Wyoming</a> but fell in love with coding from taking <a href="https://pll.harvard.edu/course/cs50-introduction-computer-science" target="_blank">CS50</a> online.</p>
+      <p className='intro'>I'm a senior newsroom developer at National Geographic. Before that I  was a developer for <a href="https://protocol.com" target="_blank">Protocol</a> (RIP) and <a href="https://www.sfchronicle.com/" target="_blank">Heasrt Newspapers</a>. I started my career reporting for a <a href="https://rivertonranger.com/" target="_blank">small newspaper in Wyoming</a> but fell in love with coding from <a href="https://pll.harvard.edu/course/cs50-introduction-computer-science" target="_blank">CS50</a>, online intro to computer science course.</p>
       <h2 className='subhead'>Some projects I'm proud of</h2>
-      <p className='disclaimer'>I sought to have the link to work around paywalls but some workarounds might stop working. Also, most of the code is proprietary so I'm showing what I can.</p>
+      {/* <p className='disclaimer'>I sought to have the link to work around paywalls but some workarounds might stop working. Also, most of the code is proprietary so I'm showing what I can.</p> */}
       <section key="top-projects">
               {highPriority.map(handleLink)}
       </section>
