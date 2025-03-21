feb86391306e4dbe9f97549c03c43d1e5eed6cbf
eric <eric.j.blom@gmail.com>
2025-03-21 16:42:29 -0500

    adds pwhl elo

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ const App = ()=>{
 
         </div>
       </section>
-      <p className='intro'>I'm a senior newsroom developer at National Geographic. Before that I  was a developer for <a href="https://protocol.com" target="_blank">Protocol</a> (RIP) and <a href="https://www.sfchronicle.com/" target="_blank">Heasrt Newspapers</a>. I started my career reporting for a <a href="https://rivertonranger.com/" target="_blank">small newspaper in Wyoming</a> but fell in love with coding from <a href="https://pll.harvard.edu/course/cs50-introduction-computer-science" target="_blank">CS50</a>, online intro to computer science course.</p>
+      <p className='intro'>I'm a geospatial data engineer at Corteva Agriscience.  Before that I  was a developer for <a href="https://www.nationalgeographic.com/" target="_blank">National Geographic</a><a href="https://protocol.com" target="_blank">Protocol</a> (RIP) and <a href="https://www.sfchronicle.com/" target="_blank">Hearst Newspapers</a>. I started my career reporting for a <a href="https://rivertonranger.com/" target="_blank">small newspaper in Wyoming</a> but fell in love with coding from <a href="https://pll.harvard.edu/course/cs50-introduction-computer-science" target="_blank">CS50</a>, online intro to computer science course.</p>
       <h2 className='subhead'>Projects I'm proud of</h2>
       {/* <p className='disclaimer'>I sought to have the link to work around paywalls but some workarounds might stop working. Also, most of the code is proprietary so I'm showing what I can.</p> */}
       <section className="top-projects">
